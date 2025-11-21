356a401407a1da448b5c682728a4d9d1a420cf4f
matheussilvano2005@gmail.com <matheussilvano2005@gmail.com>
2025-11-21 20:09:06 -0300

    feat(checkout): create CheckoutPage component and integrate checkout functionality

--- src/app/components/CheckOutButton/CheckOutButton.tsx
+++ src/app/components/CheckOutButton/CheckOutButton.tsx
@@ -1,20 +1,16 @@
 'use client'; 
 
 import React from 'react';
+import Link from 'next/link';
 
 const CheckoutButton: React.FC = () => {
-
-  const handleCheckout = () => {
-    console.log('Iniciando processo de checkout...');
-  };
-
   return (
-    <button 
-      onClick={handleCheckout}
-      className="w-full bg-green-600 text-white py-3 rounded-lg text-lg font-semibold hover:bg-green-700 transition"
+    <Link 
+      href="/checkout"
+      className="block w-full bg-green-600 text-white text-center py-3 rounded-lg text-lg font-semibold hover:bg-green-700 transition"
     >
       Finalizar Compra
-    </button>
+    </Link>
   );
 };
 

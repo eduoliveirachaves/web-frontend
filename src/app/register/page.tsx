edfd57a9c719e6b475cb7a674c3fb85f48db2e69
Guilherme Mulatinho Battisti <guimulatinho@gmail.com>
2025-11-21 19:21:02 -0300

    feat: login and register pages

---
+++ src/app/register/page.tsx
@@ -0,0 +1,9 @@
+import RegisterForm from "../components/RegisterForm/RegisterForm";
+
+export default function RegisterPage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
+      <RegisterForm />
+    </main>
+  );
+}

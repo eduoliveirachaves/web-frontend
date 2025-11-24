4a05600938b86a09219d4334fa0f9fb087cb6fa3
Eduardo Oliveira <eduardodeoliveirachaves7@gmail.com>
2025-11-23 22:36:16 -0300

    fix: Ajusta acesso de env no cart service

--- src/app/services/wishlistService.ts
+++ src/app/services/wishlistService.ts
@@ -1,6 +1,6 @@
 import { Product } from '@/app/types/';
 
-const BACKEND_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001';
+const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:3001';
 
 export const wishlistService = {
 	// Todos os itens da wishlist
